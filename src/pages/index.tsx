aa87474bcf0404cab9dc02de515e3728ae52684f
beanziii <93879641+beanziii@users.noreply.github.com>
2023-02-16 20:09:27 +0000

    basic login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Home: NextPage = () => {
+  const { data, status } = useSession();
+
+  console.log("data: ", data);
+  console.log("status: ", status);
+
+  const handleSignIn = () => {
+    signIn("discord").catch((err) => console.error(err));
+  };
+
+  const handleSignOut = () => {
+    signOut().catch((err) => console.error(err));
+  };
+
   return (
     <>
       <Head>
@@ -12,6 +27,7 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
@@ -21,7 +37,28 @@ const Home: NextPage = () => {
               knowledge. You will get placed at a random stadium around Europe
               and you will have to guess various facts about it.
             </p>
-            <button className="btn-primary btn">Login with discord</button>
+
+            <div className="flex items-center justify-center gap-4">
+              {status === "loading" || status === "unauthenticated" ? (
+                <button disabled className="btn-disabled btn">
+                  Play now
+                </button>
+              ) : (
+                <Link href={"/play"} className="btn-primary btn">
+                  Play now
+                </Link>
+              )}
+
+              {status === "loading" || status === "unauthenticated" ? (
+                <button className="btn-secondary btn" onClick={handleSignIn}>
+                  Login with discord
+                </button>
+              ) : (
+                <button className="btn-warning btn" onClick={handleSignOut}>
+                  Sign Out
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </main>
---
+++ src/pages/play/index.tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from "next";
+
+const Play: NextPage = () => {
+  return (
+    <main>
+      <h1 className="text-4xl">This is the game page</h1>
+    </main>
+  );
+};
+
+export default Play;
