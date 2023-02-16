2df98aec386a2cc0ae858405977b015e914a1db2
beanziii <93879641+beanziii@users.noreply.github.com>
2023-02-16 16:31:47 +0000

    added hero

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html data-theme="business">
+      <Head />
+      <body className="min-h-screen">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
