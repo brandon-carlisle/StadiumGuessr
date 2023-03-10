6f76aef0e3b0c7315b5aa601f49d7e5f162b51da
beanziii <93879641+beanziii@users.noreply.github.com>
2023-03-10 18:43:09 +0000

    added loading states for page transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,52 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "../store/store";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import "../styles/globals.css";
+
+function Loading() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <progress className="progress progress-primary w-56"></progress>
+    </div>
+  );
+}
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleStart = (url: string) =>
+      url !== router.asPath && setLoading(true);
+
+    const handleComplete = () => setLoading(false);
+
+    // const handleComplete = () =>
+    //   setTimeout(() => {
+    //     setLoading(false);
+    //   }, 500);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
+
   return (
     <SessionProvider session={session}>
       <ReduxProvider store={store}>
-        <Component {...pageProps} />
+        {loading ? <Loading /> : <Component {...pageProps} />}
       </ReduxProvider>
     </SessionProvider>
   );
