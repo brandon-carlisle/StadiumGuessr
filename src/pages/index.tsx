8e9c5610af3ef521cff123551c86b0edb08f909d
Brandon <93879641+beanziii@users.noreply.github.com>
2023-03-13 19:47:12 +0000

    refactored authed ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import { resetGame } from "../store/features/game/game-slice";
 import { getServerAuthSession } from "../server/auth";
 import type { GetServerSidePropsContext } from "next";
-import type { Session, User } from "@prisma/client";
+import type { User } from "@prisma/client";
 
 // TODO: Check auth server-side instead of useSession
 
@@ -53,44 +53,42 @@ function Home({ auth }: HomeProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="hero relative min-h-screen bg-base-200">
+      <main className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">StadiumGuessr</h1>
+            <h1 className="text-3xl font-bold md:text-5xl">StadiumGuessr</h1>
             <p className="py-6">
               StadiumGuessr is a fun game to test your football stadium
               knowledge. You will get placed at a random stadium around Europe
               and you will have to guess various facts about it.
             </p>
-
-            <div className="flex items-center justify-center gap-4">
-              {!authenticated ? (
+            {!authenticated && (
+              <div className="flex items-center justify-center gap-4">
                 <button disabled className="btn-disabled btn">
                   Play now
                 </button>
-              ) : (
-                <>
-                  <Link href={"/play"} className="btn-primary btn">
-                    Play now
-                  </Link>
-
-                  <Link href={"/leaderboard"} className="btn-accent btn">
-                    Leaderboard
-                  </Link>
-                </>
-              )}
-
-              {!authenticated ? (
-                <button className="btn-secondary btn" onClick={handleSignIn}>
+                <button className="btn btn-secondary" onClick={handleSignIn}>
                   Login with discord
                 </button>
-              ) : (
-                <button className="btn-warning btn" onClick={handleSignOut}>
+              </div>
+            )}
+            {authenticated && (
+              <div className="flex flex-wrap items-center justify-center gap-4">
+                <Link href={"/play"} className="btn btn-primary">
+                  Play now
+                </Link>
+
+                <Link href={"/leaderboard"} className="btn btn-accent">
+                  Leaderboard
+                </Link>
+                <button className="btn btn-secondary" onClick={handleModal}>
+                  How to play
+                </button>
+                <button className="btn btn-warning" onClick={handleSignOut}>
                   Sign Out
                 </button>
-              )}
-            </div>
-
+              </div>
+            )}
             {authenticated && auth.user.image ? (
               <div className="mt-16 flex flex-col items-center gap-2">
                 <p>Welcome back,</p>
@@ -112,7 +110,7 @@ function Home({ auth }: HomeProps) {
                 <div className="modal modal-open">
                   <div className="modal-box relative">
                     <label
-                      className="btn-sm btn-circle btn absolute right-2 top-2"
+                      className="btn btn-sm btn-circle absolute right-2 top-2"
                       onClick={handleModal}
                     >
                       ✕
@@ -133,13 +131,6 @@ function Home({ auth }: HomeProps) {
             )}
           </div>
         </div>
-
-        <button
-          className="btn-secondary btn absolute bottom-8 left-8"
-          onClick={handleModal}
-        >
-          How to play
-        </button>
       </main>
     </>
   );
@@ -153,4 +144,17 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   return {
     props: { auth: session },
   };
+}
+
+function UnuthenticatedButtons() {
+  return (
+    <div>
+      <button disabled className="btn-disabled btn">
+        Play now
+      </button>
+      <button className="btn btn-secondary" onClick={handleSignIn}>
+        Login with discord
+      </button>
+    </div>
+  );
 }
