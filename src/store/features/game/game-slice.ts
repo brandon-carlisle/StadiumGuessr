024b1e0e1a38889bdba52e16c5e7ee54e6fb72d6
Brandon Carlisle <brandoncarlisledev@gmail.com>
2024-08-19 17:59:43 +0100

    remove useless comments

--- src/store/features/game/game-slice.ts
+++ src/store/features/game/game-slice.ts
@@ -41,24 +41,14 @@ const gameSlice = createSlice({
   name: "game",
   initialState,
   reducers: {
-    /**
-     * Takes in an array of Stadium
-     * to be set into current game state.
-     */
     setStadiums(state, action: PayloadAction<Stadium[]>) {
       state.stadiums = action.payload;
     },
 
-    /**
-     * Sets current stadium to the payload.
-     */
     setCurrentStadium(state, action: PayloadAction<Stadium>) {
       state.currentStadium = action.payload;
     },
 
-    /**
-     * Sets current stadium to the next stadium unless already at last stadium.
-     */
     incrementCurrentStadium(state) {
       const nextStadiumIndex =
         state.stadiums.findIndex(
@@ -73,69 +63,40 @@ const gameSlice = createSlice({
       } else return;
     },
 
-    /**
-     * Adds payload to the current score.
-     */
     incrementScore(state, action: PayloadAction<number>) {
       state.score += action.payload;
     },
 
-    /**
-     * Sets stadiums left based on payload.
-     */
     setStadiumsRemaining(state, action: PayloadAction<number>) {
       state.stadiumsRemaining = action.payload;
     },
 
-    /**
-     * Decrements stadiums left by 1.
-     */
     decrementStadiumsRemaining(state) {
       if (state.stadiumsRemaining > 0) state.stadiumsRemaining--;
     },
 
-    /**
-     * Decrements timeRemaining by 1.
-     */
     decrementTimeRemaining(state) {
       state.timeRemaining--;
     },
 
-    /**
-     * Resets zoom level by recreating currentStadium state.
-     *
-     * (This is hacky)
-     */
     resetZoom(state) {
       state.currentStadium = {
         ...state.currentStadium,
       };
     },
 
-    /**
-     * Sets userHasFinishedGame based on payload
-     */
     setUserHasFinishedGame(state, action: PayloadAction<boolean>) {
       state.userHasFinishedGame = action.payload;
     },
 
-    /**
-     * Adds a team id to correctStadiumIds based on payload
-     */
     addCorrectStadiumId(state, action: PayloadAction<string>) {
       state.correctStadiumIds.push(action.payload);
     },
 
-    /**
-     * Adds a team id to incorrectStadiumIds based on payload
-     */
     addIncorrectStadiumId(state, action: PayloadAction<string>) {
       state.incorrectStadiumIds.push(action.payload);
     },
 
-    /**
-     * Resets game state to inital state
-     */
     resetGame(state) {
       state.stadiums = initialState.stadiums;
       state.currentStadium = initialState.currentStadium;
