1aa8c6c70d368533cdd5f57349721ee344bb8d34
Brandon Carlisle <brandoncarlisledev@gmail.com>
2024-08-21 19:34:12 +0100

    add all epl teams

--- src/server/stadiums/index.ts
+++ src/server/stadiums/index.ts
@@ -90,6 +90,66 @@ const EPL_STADIUMS: Stadium[] = [
     locaction: { lat: 52.055, lng: 1.144722 },
     league: "EPL",
   },
+  {
+    club: "Leicester City",
+    names: ["king power stadium", "king power"],
+    locaction: { lat: 52.62040733552879, lng: -1.1421760624687984 },
+    league: "EPL",
+  },
+  {
+    club: "liverpool",
+    names: ["anfield"],
+    locaction: { lat: 53.430833, lng: -2.960833 },
+    league: "EPL",
+  },
+  {
+    club: "Manchester City",
+    names: ["etihad", "etihad stadium", "city of manchester stadium"],
+    locaction: { lat: 53.483056, lng: -2.200278 },
+    league: "EPL",
+  },
+  {
+    club: "Manchester United",
+    names: ["old trafford", "the theatre of dreams"],
+    locaction: { lat: 53.463056, lng: -2.291389 },
+    league: "EPL",
+  },
+  {
+    club: "Newcastle United",
+    names: ["st james park", "st james' park"],
+    locaction: { lat: 54.975556, lng: -1.621667 },
+    league: "EPL",
+  },
+  {
+    club: "Nottingham Forest",
+    names: ["city ground", "the city ground"],
+    locaction: { lat: 52.94, lng: -1.132778 },
+    league: "EPL",
+  },
+  {
+    club: "Southampton",
+    names: ["st mary's stadium", "st marys stadium", "st mary's", "st marys"],
+    locaction: { lat: 50.90588284886436, lng: -1.3911201722318287 },
+    league: "EPL",
+  },
+  {
+    club: "Tottenham Hotspur",
+    names: ["tottenham hotspur stadium", "new white hart lane"],
+    locaction: { lat: 51.604444, lng: -0.066389 },
+    league: "EPL",
+  },
+  {
+    club: "west ham united",
+    names: ["london stadium", "the london stadium"],
+    locaction: { lat: 51.538611, lng: -0.016389 },
+    league: "EPL",
+  },
+  {
+    club: "wolverhampton wanderers",
+    names: ["molineux", "molineux stadium"],
+    locaction: { lat: 52.590278, lng: -2.130278 },
+    league: "EPL",
+  },
 ];
 
 export const STADIUMS = [...EPL_STADIUMS];
