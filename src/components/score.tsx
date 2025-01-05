0678253240aa5c6fb9b9619eabb8b97fec86c40d
Brandon <93879641+beanziii@users.noreply.github.com>
2025-01-05 20:35:11 +0000

    not working

--- src/components/score.tsx
+++ src/components/score.tsx
@@ -1,16 +1,31 @@
+/* eslint-disable @eslint-react/hooks-extra/no-direct-set-state-in-use-effect */
 import { useAppSelector } from "@/store/hooks";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 // TODO: Change animation based
 // on score increasing or decreasing
 
+// 1) figure out if new score is > or < current score
+//
+
+type ScoreDirection = "increased" | "decreased" | "none";
+
 export function Score() {
   const score = useAppSelector((state) => state.game.score);
+  const prevScore = useRef(score);
   const [animate, setAnimate] = useState(false);
+  const [scoreDirection, setScoreDirection] = useState<ScoreDirection>("none");
 
   useEffect(() => {
-    if (score > 0) {
-      // eslint-disable-next-line @eslint-react/hooks-extra/no-direct-set-state-in-use-effect
+    if (score > prevScore.current) {
+      setScoreDirection("increased");
+    } else if (score < prevScore.current) {
+      setScoreDirection("decreased");
+    } else {
+      setScoreDirection("none");
+    }
+
+    if (score !== prevScore.current) {
       setAnimate(true);
       const timer = setTimeout(() => {
         setAnimate(false);
@@ -18,13 +33,27 @@ export function Score() {
 
       return () => clearTimeout(timer);
     }
+
+    prevScore.current = score;
   }, [score]);
+
+  console.log(scoreDirection);
+
+  function getAnimationClass() {
+    if (!animate) {
+      return "";
+    }
+
+    return scoreDirection === "increased"
+      ? "motion-preset-confetti"
+      : "motion-preset-compress";
+  }
 
   return (
     <>
       <div className="uppercase text-xs font-semibold">Score</div>
       <div
-        className={`stat-value text-5xl text-primary-content ${animate ? "motion-preset-confetti" : ""}`}
+        className={`stat-value text-5xl text-primary-content ${getAnimationClass()}`}
       >
         {score}
       </div>
