077f0d6811f3c319c527a1745637295ecf11ed8b
Brandon <93879641+beanziii@users.noreply.github.com>
2025-04-20 18:41:21 +0100

    add some data to api

---
+++ apps/api/data/leagues/index.ts
@@ -0,0 +1,5 @@
+import { EFL_CHAMPIONSHIP } from "./efl-championship";
+import { EPL } from "./epl";
+import { League } from "./types";
+
+export const allLeagues: League[] = [EPL, EFL_CHAMPIONSHIP];
